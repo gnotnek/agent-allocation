63cef7dea818375ae1ab9d6822f403dfee75ad34
Dhista Ardiansyah <56432757+gnotnek@users.noreply.github.com>
2024-08-27 16:38:52 +0700

    chore: Assign agent to room in webhook handler

--- internal/handler/webhook_handler.go
+++ internal/handler/webhook_handler.go
@@ -1,6 +1,11 @@
 package handler
 
 import (
+	"errors"
+	"fmt"
+	"net/http"
+	"strings"
+
 	"github.com/gnotnek/agent-allocation/internal/database"
 	"github.com/gnotnek/agent-allocation/internal/models"
 	"github.com/gofiber/fiber/v2"
@@ -17,45 +22,70 @@ type WebhookPayload struct {
 
 func HandleWebhook(c *fiber.Ctx) error {
 	payload := new(WebhookPayload)
+
 	if err := c.BodyParser(payload); err != nil {
-		return c.Status(400).SendString(err.Error())
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "Cannot parse JSON",
+		})
 	}
 
-	var customer models.Customer
-	database.DB.Where("email = ?", payload.Email).FirstOrCreate(&customer, models.Customer{
-		Email:  payload.Email,
-		Avatar: payload.AvatarURL,
-		RoomID: payload.RoomID,
-		AppID:  payload.AppID,
-		Status: "waiting",
-		Extras: payload.Extras,
+	assignedAgentID, err := assignAgentToRoom(payload)
+	if err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": err.Error(),
+		})
+	}
+
+	return c.JSON(fiber.Map{
+		"agent_id": assignedAgentID,
+		"message":  "Agent assigned successfully",
 	})
 
-	var agents []models.Agent
-	database.DB.Where("status = ?", "online").Find(&agents)
+}
 
-	var selectedAgent *models.Agent
-	for i := range agents {
-		if agents[i].CurrentCustomer < agents[i].MaxCustomer {
-			if selectedAgent == nil || agents[i].CurrentCustomer < selectedAgent.CurrentCustomer {
-				selectedAgent = &agents[i]
-			}
-		}
+func assignAgentToRoom(payload *WebhookPayload) (uint, error) {
+	var selectedAgent models.Agent
+	database.DB.Where("status = ? AND current_customer < max_customer", "online").First(&selectedAgent)
+
+	if selectedAgent.ID == 0 {
+		return 0, errors.New("no available agent")
 	}
 
-	if selectedAgent == nil {
-		return c.Status(500).JSON(fiber.Map{"error": "No suitable agent found"})
+	arr := hitAssignmentAPI(payload.RoomID, selectedAgent.ID)
+	if arr != nil {
+		return 0, arr
 	}
-
-	newAssigment := models.Assignment{
-		AgentID:    selectedAgent.ID,
-		CustomerID: string(rune(customer.ID)),
-	}
-
-	database.DB.Create(&newAssigment)
 
 	selectedAgent.CurrentCustomer++
 	database.DB.Save(&selectedAgent)
 
-	return c.JSON(fiber.Map{"agent_id": selectedAgent.ID, "message": "Agent assigned successfully"})
+	return selectedAgent.ID, nil
+}
+
+func hitAssignmentAPI(roomID string, agentID uint) error {
+	assigAgentURL := "https://multichannel.qiscus.com//api/v1/admin/service/assign_agent"
+
+	payload := strings.NewReader(fmt.Sprintf("room_id=%s&agent_id=%d&replace_latest_agent=1&max_agent=1", roomID, agentID))
+
+	req, err := http.NewRequest("POST", assigAgentURL, payload)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Qiscus-App-Id", "AppCode")
+	req.Header.Set("Qiscus-Secret-Key", "SecretKey")
+
+	httpClient := &http.Client{}
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return errors.New("error assigning agent")
+	}
+
+	return nil
 }
